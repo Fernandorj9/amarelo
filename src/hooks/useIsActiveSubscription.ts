6d5ac347bc60820e4427cc6820bee277b0cd9a14
Giovanni Martins <gmgit10@gmail.com>
2025-04-20 13:17:26 -0300

    alterando data inscrições

--- src/hooks/useIsActiveSubscription.ts
+++ src/hooks/useIsActiveSubscription.ts
@@ -13,7 +13,7 @@ export function useIsActiveSubscription() {
     count = await getCyclistSubscriptionsCount();
 
     if (count <= 3000) {
-      if(isAfter(new Date(), new Date("2025-04-11")) && isBefore(new Date(), new Date("2025-04-17"))){
+      if(isAfter(new Date(), new Date("2025-04-11")) && isBefore(new Date(), new Date("2025-04-30"))){
         setIsActiveSubscription(true);
       }
     }
